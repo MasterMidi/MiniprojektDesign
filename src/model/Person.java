7fb99fdbf28dab0c335234706bfa0d5c22226fe2
bukz <julius@juto.dk>
2020-11-12 12:34:43 +0100

    done commting person

--- src/model/Person.java
+++ src/model/Person.java
@@ -20,42 +20,83 @@ public class Person {
 		this(name, null, phoneNr, null, null);
 	}
 
+	/**
+	 * Get the name of the user
+	 * @return the name of the user
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Set the name of the user
+	 * @param name name of the user
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	
+	 /**
+	  * Gets the address from the person
+	  * @return address of this person
+	  */
 	public String getAddress() {
 		return address;
 	}
 
+	/**
+	 * Sets the address of this user
+	 * @param address new address
+	 */
 	public void setAddress(String address) {
 		this.address = address;
 	}
 
+	/**
+	 * Get the phone number for a user
+	 * @return phone number
+	 */
 	public String getPhoneNr() {
 		return phoneNr;
 	}
 
+	/**
+	 * Sets the user phone number
+	 * @param phoneNr new phone number
+	 */
 	public void setPhoneNr(String phoneNr) {
 		this.phoneNr = phoneNr;
 	}
 
+	/**
+	 * gets the postal code for this person
+	 * @return postal code for this person
+	 */
 	public String getPostalCode() {
 		return postalCode;
 	}
 
+	/**
+	 * sets the postal code for this person
+	 * @param postalCode new postal code
+	 */
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
 	}
 
+	/**
+	 * gets the city for this person
+	 * @return city 
+	 */
 	public String getCity() {
 		return city;
 	}
 
+	/**
+	 * sets the city of this person
+	 * @param city new city
+	 */
 	public void setCity(String city) {
 		this.city = city;
 	}
--- src/model/PersonContainer.java
+++ src/model/PersonContainer.java
@@ -9,6 +9,10 @@ public class PersonContainer {
 	private static PersonContainer instance;
 	private List<Person> persons;
 
+	/**
+	 * Singleton instance for the personContainer
+	 * @return singleton object of PersonContainer
+	 */
 	public static PersonContainer getInstance() {
 		if (instance == null) {
 			instance = new PersonContainer();
@@ -17,6 +21,7 @@ public class PersonContainer {
 		return instance;
 	}
 
+	
 	private PersonContainer() {
 		persons = new ArrayList<>();
 
@@ -31,6 +36,11 @@ public class PersonContainer {
 		persons.add(new Person("Alex", "24986881"));
 	}
 
+	/**
+	 * Finds persons based on search query
+	 * @param input search query
+	 * @return List of persons that match the search query
+	 */
 	public List<Person> findPersons(String input) {
 		boolean isInt = input.matches("\\d+");
 
@@ -40,6 +50,11 @@ public class PersonContainer {
 
 	}
 
+	/**
+	 * Find a person with id
+	 * @param id the id of the person to find
+	 * @return Person with the specific id
+	 */
 	public Person selectPerson(String id) {
 		Optional<Person> person = persons.stream().filter(p -> p.getPhoneNr().equals(id)).findFirst();
 
@@ -47,11 +62,26 @@ public class PersonContainer {
 
 	}
 
+	/**
+	 * deletes a person with the specific id
+	 * @param id id of the person to delete
+	 */
 	public void deletePerson(String id) {
 		persons.remove(selectPerson(id));
 
 	}
 
+	/**
+	 * Updates a person
+	 * if value are null of empty, the current value will be used 
+	 * @param id id of the person to update
+	 * @param name new name to the updated person
+	 * @param address new address for the person
+	 * @param phoneNr new phone number for the person
+	 * @param postalCode new postal code for the person
+	 * @param city new city for the person
+	 * @return
+	 */
 	public boolean updatePerson(String id, String name, String address, String phoneNr, String postalCode,String city) {
 		try {
 			int index = persons.indexOf(selectPerson(id));
@@ -77,6 +107,11 @@ public class PersonContainer {
 
 	}
 
+	/**
+	 * Adds a person to the 'persons' list
+	 * @param person person to add
+	 * @return true/false based on success.
+	 */
 	public boolean addPerson(Person person) {
 		return persons.add(person);
 	}
